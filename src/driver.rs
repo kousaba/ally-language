4fe9fc5235ca709ed54745e437eaba3f8b81f3dd
kousaba <kousaba1@proton.me>
2026-03-15 14:54:34 +0900

    時間測定を追加

--- src/driver.rs
+++ src/driver.rs
@@ -2,15 +2,21 @@ use crate::session::Session;
 use crate::lexer::{Lexer, AllyToken};
 use crate::ast::base::Module;
 use crate::parser::Parser;
+use std::time::Instant;
 use anyhow::Result;
 
 pub fn compile(sess: &mut Session, source: &str) -> Result<()> {
+    let mut start = Instant::now();
     let token = lexer(sess, source);
+    let lexer_duration = start.elapsed();
+    start = Instant::now();
     let ast = parser(sess, token);
+    let parser_duration = start.elapsed();
     dbg!(ast);
+    println!("Lexer: {:?}ms", lexer_duration.as_millis());
+    println!("Parser: {:?}ms", parser_duration.as_millis());
     Ok(())
 }
-
 
 fn lexer(sess: &mut Session, source: &str) -> Vec<AllyToken>{
     let mut lexer = Lexer::new(source);
@@ -18,12 +24,6 @@ fn lexer(sess: &mut Session, source: &str) -> Vec<AllyToken>{
 
     for token in &tokens{
         println!("Token: {token:?}");
-        /*match token.kind{
-            crate::lexer::AllyTokenKind::Identifier(sym) => {
-                println!("Identifier: {} (ID: {:?})", sess.lookup(sym), sym);
-            }
-            other => println!("Token: {other:?}"),
-        }*/
     }
     
     tokens
